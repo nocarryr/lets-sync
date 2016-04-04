84246e14da99d412ad50d35f23cbd21c24e35bab
nocarryr <matt@nomadic-recording.com>
2016-04-04 13:16:10 -0500

    Add test for tree copy

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -2,3 +2,10 @@
 def test_build_tree(conf_dir):
     from letssync.structures import build_tree
     r = build_tree(str(conf_dir['root_path']))
+
+def test_copy(conf_dir):
+    from letssync.structures import build_tree
+    r1 = build_tree(str(conf_dir['root_path']))
+    r2 = r1.copy()
+    assert r1.is_equal(r2)
+    assert r2.is_equal(r1)
