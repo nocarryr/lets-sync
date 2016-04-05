f05e32f85cd4bb440f0417a2dd1bb926688a8939
nocarryr <matt@nomadic-recording.com>
2016-04-05 09:50:52 -0500

    Add tests for search methods and overwrite protection

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -20,3 +20,26 @@ def test_rw(conf_dir, tmpdir_factory):
     r3 = build_tree(str(t))
     assert r1.is_equal(r3) and r3.is_equal(r1)
     assert r2.is_equal(r3) and r3.is_equal(r2)
+
+def test_search(conf_dir):
+    from letssync.structures import build_tree
+    r1 = build_tree(str(conf_dir['root_path']))
+    accounts = r1.search('accounts')
+    account_id = conf_dir['account_id']
+    account = accounts.accounts[account_id]
+    search_account = r1.search(account.relative_path)
+    assert account == search_account
+
+def test_overwrite_protection(conf_dir, tmpdir_factory):
+    from letssync.structures import build_tree
+    r1 = build_tree(str(conf_dir['root_path']))
+    accounts = r1.search('accounts')
+    account_id = conf_dir['account_id']
+    account = accounts.accounts[account_id]
+    meta = account.search('meta.json')
+    prev_content = meta.content
+    meta.content = ''
+    r1.write(overwrite=False, recursive=True)
+    with open(meta.path, 'r') as f:
+        s = f.read()
+    assert s == prev_content
