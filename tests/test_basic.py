b6c6f44565592aa0ede8b0e5f186a75da4309a9c
nocarryr <matt@nomadic-recording.com>
2016-04-05 09:40:42 -0500

    Add test for write method

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,4 @@
+import os
 
 def test_build_tree(conf_dir):
     from letssync.structures import build_tree
@@ -9,3 +10,13 @@ def test_copy(conf_dir):
     r2 = r1.copy()
     assert r1.is_equal(r2)
     assert r2.is_equal(r1)
+
+def test_rw(conf_dir, tmpdir_factory):
+    from letssync.structures import build_tree
+    t = tmpdir_factory.mktemp('copy')
+    r1 = build_tree(str(conf_dir['root_path']))
+    r2 = r1.copy(str(t))
+    r2.write(overwrite=False, recursive=True)
+    r3 = build_tree(str(t))
+    assert r1.is_equal(r3) and r3.is_equal(r1)
+    assert r2.is_equal(r3) and r3.is_equal(r2)
